d805e9d1abd3ccc9d54307d472d0b13be6513b98
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-03-24 00:09:49 -0400

    objects, constructor properties

---
+++ objects-5-constructor-property.js
@@ -0,0 +1,17 @@
+
+
+/*
+
+- every object in js has a property called 'constructor'
+- constructor represent the function  to construct or create obj
+- blue f, function, first letter uppercase = built in constructor function
+- when obj created using the object literal syntax, internally js engine using the following constructor function 
+- new String(); is  , '', ``
+- new Boolean(); is true or false, boolean literals
+-  new Number();, number literals: 1,2,3 ... 
+
+*/
+
+let x = {};
+// let x = new Object{}
+console.log(x);
