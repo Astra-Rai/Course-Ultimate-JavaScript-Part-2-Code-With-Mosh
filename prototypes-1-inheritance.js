160a821035bad52a72aaf51bc14ad4c88c22e9fa
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-04-26 11:05:08 -0400

    prototypes, inheritance, IS-A

---
+++ prototypes-1-inheritance.js
@@ -0,0 +1,26 @@
+
+
+/*
+
+Notes: 
+
+- enables to takeon the properties and methods of anthoer objects...
+- this makes it easy to reuse code in different parts of an application
+    example: Circle and Shape, both can use function: computeOptiumLocation()
+- implementation method the same for both Circle and Square
+- if there is a bug in implementation (computerOptimumFunction) or code needed to be added...
+    we have to change source code in multiple locations
+
+                                   Shape
+
+                     ( IS-A Shape ) Circle      Square  
+
+- shape 'class' : Base/Super/Paper
+- Circel and Square: Derived / Sub / Child
+- inheritance relationship referred to as IS-A
+
+- Classical vs. Prototypical Inheritance
+- JavaScript, no classes, JavaScript has Prototypical Inheritance
+
+
+ */
