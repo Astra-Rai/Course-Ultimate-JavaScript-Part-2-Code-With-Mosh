5c154c27e559f5a3b5444a509c9f207007514039
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2021-05-01 22:52:53 -0400

    __proto__,  __proto__Object

---
+++ JavaScript-Prototypes-3-Multilevel-Inheritance-Part-1/script.js
@@ -0,0 +1,15 @@
+
+let myArray =[];
+
+/*
+- []
+- length: 0
+- proto, select dropdown, methods available listed
+    __proto__
+- this prototype is the parent for all arrays in javascrip
+- myArray, in memory, 'object base (arrayBase)'
+- inspect prototype of arrayBase object
+- scroll down all the way, another proto property:
+-  __proto__: Object, this is the root object in js 'object base'
+
+*/
